59969be889ac24f286a926869da875d1b4ae3573
sause668 <soulsitescoding@zohomail.com>
2025-05-26 14:41:22 -0400

    created team stats model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,4 +4,5 @@ from .team import Team
 from .player import Player
 from .game_day import Game_Day
 from .game import Game
+from .team_stat import Team_Stat
 from .db import environment, SCHEMA
---
+++ app/models/team_stat.py
@@ -0,0 +1,21 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Team_Stat(db.Model):
+    __tablename__ = 'team_stats'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    game_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('games.id')), nullable=False, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('teams.id')), nullable=False, primary_key=True)
+    home = db.Column(db.Boolean)
+    win = db.Column(db.Boolean)
+    points = db.Column(db.Integer)
+    
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+        }
+    
+
