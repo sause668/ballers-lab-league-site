86f7c80616c61d78f9f6e9cc88d58797a5434af1
sause668 <soulsitescoding@zohomail.com>
2025-06-03 17:15:09 -0400

    bug fix

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,12 +1,13 @@
 from app.models import db, User, environment, SCHEMA
 from sqlalchemy.sql import text
+import os
 
 
 def seed_users():
 
     db.session.add(User(
-        username='livadmin', 
-        password='ballerswithsoul94',
+        username=os.environ.get("USERNAME"), 
+        password=os.environ.get("USER_PASSWORD"),
     ))
 
     db.session.commit()
