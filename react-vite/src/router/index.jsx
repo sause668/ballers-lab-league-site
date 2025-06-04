b697aa6233c39f21ddf29194eafa7c8531b00abb
sause668 <soulsitescoding@zohomail.com>
2025-06-04 14:41:16 -0400

    finished routes

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,4 +1,4 @@
-import { Navigate, createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
 
 export const router = createBrowserRouter([
@@ -7,7 +7,67 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: "Home",
+        element: (<>HomePage</>),
+      },
+      {
+        path: "league/",
+        children: [
+          {
+            path: "",
+            element: 'LeaguePage',
+          },
+          {
+            path: "rules",
+            element: 'RulesPage',
+          },
+          {
+            path: "history",
+            element: 'HistoryPage',
+          },
+        ]
+      },
+      {
+        path: "schedule/",
+        children: [
+          {
+            path: "",
+            element: 'SchedulePage',
+          },
+          {
+            path: ":gameDayId/",
+            children: [
+              {
+                path: "",
+                element: 'GameDayPage',
+              },
+              {
+                path: ":gameId/",
+                children: [
+                  {
+                    path: "",
+                    element: 'GamePage',
+                  },
+                  {
+                    path: "stats",
+                    element: 'GameStatsPage',
+                  },
+                ]
+              },
+            ]
+          },
+        ]
+      },
+      {
+        path: "photos/",
+        children: [
+          {
+            path: "",
+            element: 'PhotosPage',
+          },{
+            path: ":seasonId",
+            element: 'SeasonPhotosPage',
+          },
+        ]
       },
     ],
   },
