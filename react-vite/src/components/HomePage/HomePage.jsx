4ed267bf89314ecc4c83f662a4282058323cf265
sause668 <soulsitescoding@zohomail.com>
2026-05-05 16:25:15 -0400

    Date change

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -16,9 +16,9 @@ export default function HomePage() {
       <img id="logoHM" src={`${url}imgs/logo-main.png`} alt="" />
      </div>
      <div id="titleConHM">
-      <h1 id="t1HP" className="titleHM">Season 4</h1>
-      <h1 id="t2HP" className="titleHM">June 1st - July 27th</h1>
-      <h1 id="t3HP" className="titleHM">2025</h1>
+      <h1 id="t1HP" className="titleHM">Season 5</h1>
+      <h1 id="t2HP" className="titleHM">June 7th - August 2nd</h1>
+      <h1 id="t3HP" className="titleHM">2026</h1>
      </div>
      <div id="videoConHM">
       {/* <video id="videoHM" width="320" height="240" autoPlay loop muted>
